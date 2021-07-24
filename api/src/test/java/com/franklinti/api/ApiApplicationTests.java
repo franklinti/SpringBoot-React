0b053c18d8682d209da51a90659db28c2dc23d28
DESKTOP-81P0RNM\frank <franklinti@outlook.com>
2021-07-24 15:06:55 -0300

    Primeiro commit adicionado projeto

---
+++ api/src/test/java/com/franklinti/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franklinti.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
